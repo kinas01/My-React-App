3ca6fa8fb1098bc7eb1fc14eee234ded413e347f
Kinas01 <bestmedi50@gmail.com>
2025-11-22 09:06:59 +0100

    Updated content and make State component functional

--- src/practice/State.jsx
+++ src/practice/State.jsx
@@ -7,11 +7,13 @@ function State() {
         <div>
             <h2>Set counting</h2>
 
+             <p>Count: {count}</p>
+
             <button onClick={ () => setcount(count + 1)}>Increase</button>
 
             <button onClick={ () => setcount(count - 1)} style={{ marginLeft: "10px"}}>decrease</button>
 
-            <button onClick={ () => setcount(0)}  style={{ marginLeft: "10px"}}>councel </button>
+            <button onClick={ () => setcount(0)}  style={{ marginLeft: "10px"}}>Reset </button>
 
         </div>
     );
